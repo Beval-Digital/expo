368c47f8ff4a2eec32d2307a19105342fbd45cca
Eric Valentino <104712158+Ourve76@users.noreply.github.com>
2024-06-07 05:40:14 +0700

    Update views.py

--- machine_learning/views.py
+++ machine_learning/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
