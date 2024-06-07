605e02559152908a6d2d317fe427253883ec2d96
Eric Valentino <104712158+Ourve76@users.noreply.github.com>
2024-06-07 23:07:00 +0700

    Update settings.py

--- expo/settings.py
+++ expo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7n534)bsd^!t1gwx@i6lxk*6vit&488!jr^^ka&1-+49im$9#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
